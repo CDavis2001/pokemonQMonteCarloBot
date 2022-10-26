1a232f7b18a6209f7a4d406755d6a97cc7e88fcd
Christopher Davis <candavis01@gmail.com>
2022-10-26 14:53:49 +0100

    add test file and method to create random player

--- testCrossEvaluate.py
+++ testCrossEvaluate.py
@@ -1,24 +1,23 @@
 import asyncio
-
-from poke_env.player import cross_evaluate, RandomPlayer
 from tabulate import tabulate
+from poke_env.player import cross_evaluate
+from RandomAgent import makeRandomPlayer
 
 async def main():
-    players = [
-        RandomPlayer(max_concurrent_battles=10) for _ in range(3)
-    ]
-    cross_evaluation = await cross_evaluate(players,n_challenges=20)
+    players = []
+    for i in range(0,5):
+        players.append(makeRandomPlayer())
+        
+    challenges = 10
+    
+    
+    cross_evaluation = await cross_evaluate(players,challenges)
     table = [["-"] + [p.username for p in players]]
     for p_1, results in cross_evaluation.items():
         table.append([p_1] + [cross_evaluation[p_1][p_2] for p_2 in results])
         
     print(tabulate(table))
-
+    
 if __name__ == "__main__":
     asyncio.get_event_loop().run_until_complete(main())
-    
-    
-def makeRandomPlayer():
-    return RandomPlayer(max_concurrent_battles=10)
-
     
