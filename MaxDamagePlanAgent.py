9b0f080d73151cb8f5476f9508511ce1a64e7967
Christopher Davis <candavis01@gmail.com>
2022-11-02 15:41:34 +0000

    added type matchup checker

--- MaxDamagePlanAgent.py
+++ MaxDamagePlanAgent.py
@@ -1,8 +1,20 @@
 from poke_env.player import Player
+from poke_env.environment import Battle
+from poke_env.environment.pokemon import Gen8Pokemon
 
-class MaxDamagePlayer(Player):
+class MaxDamagePlanPlayer(Player):
     
     def choose_move(self, battle):
+        Active = battle.active_pokemon
+        OpActive = battle.opponent_active_pokemon
+        
+        for Type in OpActive.types:
+            if Active.damage_multipler() > 1.0:
+                print ("bad matchup")
+                
+            
+        
+        
         # If the player can attack, it will
         if battle.available_moves:
             # Finds the best move among available ones
