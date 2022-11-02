27297975c13d2d4d11e2672c38e1a87cb0670756
Christopher Davis <candavis01@gmail.com>
2022-11-02 19:56:31 +0000

    fixed damage multipler calculator

--- MaxDamagePlanAgent.py
+++ MaxDamagePlanAgent.py
@@ -1,21 +1,27 @@
 from poke_env.player import Player
 from poke_env.environment import Battle
-from poke_env.environment.pokemon import Gen8Pokemon
+from poke_env.environment import Pokemon
 
-class MaxDamagePlanPlayer(Player):
+class MaxDamagePlanPlayer(Player):  
     
     def choose_move(self, battle):
+        
+        # Have a 10% chance to act completely randomly to simulate
+        # a learning agent exploring
+        
         Active = battle.active_pokemon
         OpActive = battle.opponent_active_pokemon
         
-        for Type in OpActive.types:
-            if Active.damage_multipler() > 1.0:
-                print ("bad matchup")
+        
+        if Active.damage_multiplier(OpActive.type_1) > 1.0:
+            print ("bad matchup")
+            # switch
                 
             
         
         
-        # If the player can attack, it will
+        # If not switching, pick the move with the highest effective
+        # base power
         if battle.available_moves:
             # Finds the best move among available ones
            best_move = max(battle.available_moves, key = lambda move: move.base_power)
