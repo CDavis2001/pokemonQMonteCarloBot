42b03b6143b4a38a3828642766f11795e5f0c8bb
Christopher Davis <candavis01@gmail.com>
2022-11-24 16:40:19 +0000

    added calcMatchUpModifier

---
+++ utility.py
@@ -0,0 +1,15 @@
+from poke_env.environment import Battle
+from poke_env.environment import Pokemon
+
+def calcPokeMatchUpModifier(Poke1, Poke2):
+    # modifier for Poke1 stab attacks on Poke 2
+    modifier = 1
+    modifier = modifier * Poke2.damage_multiplier(Poke1.type_1)
+    if Poke1.type_2:
+        modifier = modifier * Poke2.damage_multiplier(Poke1.type_2)
+        
+    # modifier for Poke2 stab attacks on Poke 1
+    modifier = modifier * Poke1.damage_multiplier(Poke2.type_1)
+    if Poke2.type_2:
+        modifier = modifier * Poke1.damage_multiplier(Poke2.type_2)
+    
