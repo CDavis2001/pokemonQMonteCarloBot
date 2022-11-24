86ab4ec2ebc3cef6e09628fc16adf4dd2f42a97e
Christopher Davis <candavis01@gmail.com>
2022-11-24 16:40:03 +0000

    Implemented embed_battle()

--- QLearningAgent.py
+++ QLearningAgent.py
@@ -1,23 +1,43 @@
 from poke_env.player import Player
 from poke_env.environment import Battle
 from poke_env.environment import Pokemon
+from utility import *
 
 class QLearningPlayer(Player):
     
     # self, battle -> move order
     # creates a move order to send to the server
     def choose_move(self, battle):
+        # embed battle
+        
+        # check if 
         return None
     
     # self, battle -> observation
-    # converts specific battle instance into generic battle class
+    # converts specific battle instance into observation
     def embed_battle(self, battle):
         # attributes we want to track
-        # type matchup modifier, effective base powers, type matchup modifiers of team mates (-1 for fainted)
+        # type matchup modifier, effective base powers
         observation = []
         index = 0
-        observation[index]
-        return None
+        observation[index] = calcPokeMatchUpModifier(battle.active_pokemon, battle.opponent_active_pokemon)
+        index+=1
+        observation[index] = len(battle.available_switches())
+        index+=1
+        i = 0
+        for move in battle.available_moves():
+            base_power = move.base_power
+            # check for same type attack bonus
+            if move.type == battle.active_pokemon.type_1:
+               base_power = base_power * 1.5
+            elif battle.active_pokemon.type_2:
+                if move.type == battle.active_pokemon.type_2:
+                   base_power = base_power * 1.5
+            base_power = base_power * battle.opponent_active_pokemon.damage_multiplier(move.type)
+            observation[index + i]
+            i+=1
+            
+        return observation
     
     # self, battle, move -> float
     def get_utility(self, battle, move):
