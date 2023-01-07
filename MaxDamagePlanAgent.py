961d99d7cc05a8c0dbafa3fdd2a64a4fd6a3c6aa
Christopher Davis <candavis01@gmail.com>
2023-01-07 12:28:43 +0000

    added team preview methods for OU format

--- MaxDamagePlanAgent.py
+++ MaxDamagePlanAgent.py
@@ -1,6 +1,7 @@
 from poke_env.player import Player
 from poke_env.environment import Battle
 from poke_env.environment import Pokemon
+import numpy as np
 
 class MaxDamagePlanPlayer(Player):  
     
@@ -16,14 +17,15 @@ class MaxDamagePlanPlayer(Player):
         if Active.damage_multiplier(OpActive.type_1) > 1.0:
             print ("bad matchup")
             # switch
-            possible_switches = battle.available_switches()
+            possible_switches = battle.available_switches
             modifier = 4
-            
+            switch = None
             for poke in possible_switches:
                 if poke.damage_multiplier(OpActive.type_1) < modifier:
                     switch = poke
                     modifier = poke.damage_multiplier(OpActive.type_1)
-            return self.create_order(switch)
+            if switch != None:
+                return self.create_order(switch)
         
         # If not switching, pick the move with the highest effective
         # base power
@@ -52,5 +54,41 @@ class MaxDamagePlanPlayer(Player):
        
         else:
             return self.choose_random_move(battle)
+       
+    def teampreview(self, battle):
+        pkmn_matchup = {}
+
+        # For each of our pokemon
+        for i, pkmn in enumerate(battle.team.values()):
+            # We store their average performance against the opponent team
+            pkmn_matchup[i] = np.mean([
+                MaxDamagePlanPlayer.teampreview_performance(pkmn, opp)
+                for opp in battle.opponent_team.values()
+            ])
+
+        # We sort our pokemon by performance
+        ordered_mons = sorted(pkmn_matchup, key = lambda k: -pkmn_matchup[k])
+
+        # We start with the one we consider best overall
+        # We use i + 1 as python indexes start from 0
+        #  but showdown's indexes start from 1
+        return "/team " + ''.join([str(i + 1) for i in ordered_mons])
+    
+    def teampreview_performance(pkmn1, pkmn2):
+        # calc offensive matchup multiplier for each pokemon against the other
+        pkmn1vs2 = pkmn1.damage_multiplier(pkmn2.type_1)
+        if pkmn2.type_2:
+            pkmn1vs2 = max(pkmn1vs2, pkmn1.damage_multiplier(pkmn2.type_2))
+        
+        pkmn2vs1 = pkmn2.damage_multiplier(pkmn1.type_1)
+        if pkmn1.type_2:
+            pkmn2vs1 = max(pkmn2vs1, pkmn2.damage_multiplier(pkmn1.type_2))
+        
+        # return difference 
+        return pkmn2vs1 - pkmn1vs2
+    
+    
+        
+        
         
   
