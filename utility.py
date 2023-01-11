d8aabe7f5a68ef2822614ed3474f3d91ff6c8634
Christopher Davis <candavis01@gmail.com>
2023-01-11 15:16:03 +0000

    remove superfluous state utility calc function

--- utility.py
+++ utility.py
@@ -12,22 +12,3 @@ def calcPokeMatchUpModifier(Poke1, Poke2):
     modifier = modifier * Poke1.damage_multiplier(Poke2.type_1)
     if Poke2.type_2:
         modifier = modifier * Poke1.damage_multiplier(Poke2.type_2)
-    
-    
-def calc_utility_of_state(battle):
-    utility = 0
-    
-    team = battle._team
-    op_team = battle._opponent_team
-    
-    for pokmon in team:
-        if not pokmon[1].fainted():
-            utility += 100
-    
-    for pokmon in op_team:
-        if pokmon[1].fainted():
-            utility += 100
-    
-    
-    
-    return utility
