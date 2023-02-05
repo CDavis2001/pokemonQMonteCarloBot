f3b4eb054ccaa0b00cac92fe1ba963d13340e6c0
Christopher Davis <candavis01@gmail.com>
2023-02-05 18:17:41 +0000

    version of cross evaluate for data collection

---
+++ test1v1.py
@@ -0,0 +1,37 @@
+from poke_env.player.player import Player
+from poke_env.data import to_id_str
+from typing import Dict
+from typing import List
+from typing import Optional
+from typing import Tuple
+from concurrent.futures import Future
+import asyncio
+
+async def onev1_evaluate(
+    players: List[Player], n_challenges: int
+):
+    
+    for i, p_1 in enumerate(players):
+        for j, p_2 in enumerate(players):
+            if j <= i:
+                continue
+            await asyncio.gather(
+                p_1.send_challenges(
+                    opponent=to_id_str(p_2.username),
+                    n_challenges=n_challenges,
+                    to_wait=p_2.logged_in,
+                ),
+                p_2.accept_challenges(
+                    opponent=to_id_str(p_1.username), n_challenges=n_challenges
+                ),
+            )
+            results = dict()
+            results[p_1.username] = [p_1.win_rate, n_challenges]
+            results[p_2.username] = [p_2.win_rate, n_challenges]
+            
+            
+
+
+            p_1.reset_battles()
+            p_2.reset_battles()
+    return results  # pyre-ignore
