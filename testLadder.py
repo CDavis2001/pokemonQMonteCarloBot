c59e32060197c135ef0a952c58645818d5a61efc
= <candavis01@gmail.com>
2023-03-14 15:34:12 +0000

    added all agents for testing

--- testLadder.py
+++ testLadder.py
@@ -19,12 +19,37 @@ async def main():
         server_configuration=ShowdownServerConfiguration,
         battle_format="gen8ou", team=team, start_timer_on_battle_start=True
     )
-    """
+    
     player = MaxDamagePlanPlayer(
         player_configuration=PlayerConfiguration("qmonte1", "Neptunium237"),
         server_configuration=ShowdownServerConfiguration,
         battle_format="gen8ou", team=team, start_timer_on_battle_start=True
     )
+    
+    player = QLearningLitePlayer(
+        player_configuration=PlayerConfiguration("qmonte2", "ProjectTest2"),
+        server_configuration=ShowdownServerConfiguration,
+        battle_format="gen8ou", team=team, start_timer_on_battle_start=True
+    )
+    
+    player = HybridHPSwitchPlayer(
+        player_configuration=PlayerConfiguration("qmonte3", "ProjectTest3"),
+        server_configuration=ShowdownServerConfiguration,
+        battle_format="gen8ou", team=team, start_timer_on_battle_start=True
+    )
+    
+    player = HybridTurnSwitchPlayer(
+        player_configuration=PlayerConfiguration("qmonte4", "ProjectTest4"),
+        server_configuration=ShowdownServerConfiguration,
+        battle_format="gen8ou", team=team, start_timer_on_battle_start=True
+    )
+    """
+    player = HybridInfoSwitchPlayer(
+        player_configuration=PlayerConfiguration("qmonte5", "ProjectTest5"),
+        server_configuration=ShowdownServerConfiguration,
+        battle_format="gen8ou", team=team, start_timer_on_battle_start=True
+    )
+
     # Playing 5 games on the ladder
     await player.ladder(100)
 
